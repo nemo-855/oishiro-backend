ea096570221f3a5922a90882721150d3b115bbcb
takafumi11 <noirit7@gmail.com>
2024-01-02 22:37:01 +0900

    Added docker-comose.yml, Dockerfile

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	runtimeOnly("com.mysql:mysql-connector-j")
+	runtimeOnly("com.mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/com/nemo/oishiro/oishirobackend/service/PostService.kt
+++ src/main/kotlin/com/nemo/oishiro/oishirobackend/service/PostService.kt
@@ -1,24 +1,12 @@
 package com.nemo.oishiro.oishirobackend.service
 
 import com.nemo.oishiro.oishirobackend.model.Post
+import com.nemo.oishiro.oishirobackend.repository.PostRepository
 import org.springframework.stereotype.Service
 
 @Service
-class PostService {
+class PostService(private val postRepository: PostRepository) {
     fun getPosts(): List<Post> {
-        val posts = listOf(
-			Post(
-				id = 1,
-				deviceId = "1234567890",
-				userId = 1,
-				title = "タイトル",
-				body = "本文",
-				memo = "メモ",
-				rating = 3,
-				createdAt = "2019-01-01 00:00:00",
-				updatedAt = "2019-01-01 00:00:00"
-			)
-		)
-		return posts
+        return postRepository.findAll()
     }
 }

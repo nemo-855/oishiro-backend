6f4f08184214ca6c8dea9cf9b27ebe0ebf47297f
takafumi11 <noirit7@gmail.com>
2024-01-21 22:33:55 +0900

    Created .sql files to create, insert table, values into db

---
+++ src/main/resources/sql/insert-init-value.sql
@@ -0,0 +1,7 @@
+INSERT INTO post (device_id, user_id, title, body, memo, rating, created_at, updated_at)
+VALUES 
+('device1', 1, 'Sample Title 1', 'Sample Body 1', 'Sample Memo 1', 5, '2022-01-01 00:00:00', '2022-01-01 00:00:00'),
+('device2', 2, 'Sample Title 2', 'Sample Body 2', 'Sample Memo 2', 4, '2022-01-02 00:00:00', '2022-01-02 00:00:00'),
+('device3', 3, 'Sample Title 3', 'Sample Body 3', 'Sample Memo 3', 3, '2022-01-03 00:00:00', '2022-01-03 00:00:00'),
+('device4', 4, 'Sample Title 4', 'Sample Body 4', 'Sample Memo 4', 2, '2022-01-04 00:00:00', '2022-01-04 00:00:00'),
+('device5', 5, 'Sample Title 5', 'Sample Body 5', 'Sample Memo 5', 1, '2022-01-05 00:00:00', '2022-01-05 00:00:00');
